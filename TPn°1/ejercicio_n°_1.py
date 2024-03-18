82d1636a2032edc4563638d42b16a13948753eac
jose-ruti <joseruti66@gmail.com>
2024-03-18 19:45:00 -0300

    tp ...

--- TPn°1/ejercicio_n°_1.py
+++ TPn°1/ejercicio_n°_1.py
@@ -1,9 +1,39 @@
 import re 
 
 def validate_string(string):
-    if re.fullmatch(r"([a-zA-Z]|[0-9]){8,}?", string):
-        return True
-    else:
-        return False
+    if re.fullmatch(r"([a-zA-Z]|[0-9])?", string):
+        print ('true')
     
-print(validate_string("JoseRUT1123123"))
+    else:
+        print ('false')
+
+    if re.fullmatch(r"([a-zA-Z])", string):
+        print ('true')
+    else:
+        print ('false')
+    if re.fullmatch(r"([A-Z])", string):
+        print ('true')
+    else:
+        print ('false')
+
+    if re.fullmatch(r"([a-z])", string):
+        print ('true')
+    else:
+        print ('false')
+
+    if re.fullmatch(r"([0-9])", string):
+        print ('true')
+    else:
+        print ('false')
+    
+    if re.fullmatch(r"({8,})", string):
+        print ('true')
+    else:
+        print ('false')
+
+    
+    
+
+
+    
+print(validate_string)("JoseRUT1123123"))
