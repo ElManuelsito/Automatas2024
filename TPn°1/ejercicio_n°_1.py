c00de9efd4019206aa27472008213d9db3c822de
Augusto <au.castro@alumno.um.edu.ar>
2024-03-25 18:26:56 -0300

    Ejercicio 1 - Terminado

--- TPn°1/ejercicio_n°_1.py
+++ TPn°1/ejercicio_n°_1.py
@@ -1,39 +1,14 @@
-import re 
+import re
 
 def validate_string(string):
-    if re.fullmatch(r"([a-zA-Z]|[0-9])?", string):
-        print ('true')
-    
-    else:
-        print ('false')
+    results = []
+    results.append(bool(re.search(r"[a-zA-Z0-9]", string)))
+    results.append(bool(re.search(r"[a-zA-Z]", string)))
+    results.append(bool(re.search(r"[A-Z]", string)))
+    results.append(bool(re.search(r"[a-z]", string)))
+    results.append(bool(re.search(r"\d", string)))
+    results.append(bool(re.search(r"({8, })", string)))
+    return results
 
-    if re.fullmatch(r"([a-zA-Z])", string):
-        print ('true')
-    else:
-        print ('false')
-    if re.fullmatch(r"([A-Z])", string):
-        print ('true')
-    else:
-        print ('false')
-
-    if re.fullmatch(r"([a-z])", string):
-        print ('true')
-    else:
-        print ('false')
-
-    if re.fullmatch(r"([0-9])", string):
-        print ('true')
-    else:
-        print ('false')
-    
-    if re.fullmatch(r"({8,})", string):
-        print ('true')
-    else:
-        print ('false')
-
-    
-    
-
-
-    
-print(validate_string)("JoseRUT1123123")
+resultado = validate_string("Au")
+print("\n".join(map(str, resultado)))
