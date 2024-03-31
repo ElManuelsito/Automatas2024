0b0d87df03b7f748f0b3c20cbfd377ec240b1260
Augusto <au.castro@alumno.um.edu.ar>
2024-03-31 18:19:06 -0300

    Terminado

--- TPn°1/ejercicio_n2.py
+++ TPn°1/ejercicio_n2.py
@@ -18,16 +18,18 @@ def solve(string):
     if '(' in string or ')' in string:
         raise ValueError("La ecuación no puede contener paréntesis.")()
     
-
     if '-' in string:
-        raise ValueError("La ecuacion no puede tener numero negativo")
+        raise ValueError("La ecuacion no puede tener una resta")
+    
+    if '/' in string:
+        raise ValueError("La ecuacion no puede tener una división")
 
     math_char_list = re.findall(r'\b(?:\d+|[+*])\b', string)  
 
     formula = " ".join(math_char_list)
     resultado = eval(formula)
     print("Expresión resuelta:", resultado)
-  
+
 try:
     solve(str(input("Ingrese una ecuación : ")))
 except ValueError as e:
