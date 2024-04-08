216734bfedcc8f10272adc37634539404821ca94
jose-ruti <joseruti66@gmail.com>
2024-04-08 19:07:20 -0300

    eliminacion del except

--- TPn°1/ejercicio_n2.py
+++ TPn°1/ejercicio_n2.py
@@ -1,15 +1,3 @@
-# import re
-
-# def solve(string):
-#     # falta excepcionar el menos "-" y cualquier numero despues de ese menos
-#     math_char_list = re.findall(r"[^/-]*\d+|[+*]",string)
-#     print(math_char_list)
-#     formula = " ".join(math_char_list)
-#     print(formula)
-#     print(eval(formula))
-
-# solve(str(input("Ingrese una ecuación: ")))
-
 import re
 
 def solve(string):
@@ -18,17 +6,17 @@ def solve(string):
     if '(' in string or ')' in string:
         raise ValueError("La ecuación no puede contener paréntesis.")()
     
-
     if '-' in string:
-        raise ValueError("La ecuacion no puede tener numero negativo")
+        raise ValueError("La ecuacion no puede tener una resta")
+    
+    if '/' in string:
+        raise ValueError("La ecuacion no puede tener una división")
 
     math_char_list = re.findall(r'\b(?:\d+|[+*])\b', string)  
 
     formula = " ".join(math_char_list)
     resultado = eval(formula)
     print("Expresión resuelta:", resultado)
-  
-try:
-    solve(str(input("Ingrese una ecuación : ")))
-except ValueError as e:
-    print(e)
+
+solve(str(input("Ingrese una ecuación : ")))
+
