12926f92092038f0cde17658318ea7bbb9e4a612
Augusto <au.castro@alumno.um.edu.ar>
2024-05-22 00:14:53 -0300

    edit point4

--- TrabajoFinal/main.py
+++ TrabajoFinal/main.py
@@ -2,13 +2,13 @@ import pandas as pd, os, time, pathlib, csv, re
 from point1 import search_song
 # from point2 import search_artist
 # from point3 import add_song
-# from point4 import top_artists
+from point4 import top_artists
 import options
 
 os.system('cls')
 print(f"{options.BLUE}Reading File...")
 # Ruta Augusto
-data = pd.read_csv("/spotify_and_youtube 2024.csv")
+# data = pd.read_csv("C:/Users/acast/OneDrive/Documents/UM/2024/Automátas y Gramáticas/Automatas2024/TrabajoFinal/spotify_and_youtube 2024.csv")
 
 def parse_csv() -> list:
     songs = []
--- TrabajoFinal/point4.py
+++ TrabajoFinal/point4.py
@@ -1,13 +1,23 @@
 
-def top_artists(songs: list, artist: str) -> None:
-    albums = {}
-    for song in songs:
-        if artist.lower() in song.artist.lower():
-            artist = song.artist
-            if song.album in albums:
-                albums[song.album] = (albums[song.album][0] + 1, albums[song.album][1] + float(song.duration))
-            else:
-                albums[song.album] = (1, float(song.duration))
-    print(f"{artist} tiene {len(albums.keys())} albums:")
-    for album, songs_duration in albums.items():
-        print("\u2022", f"{album} tiene {songs_duration[0]} canciones y dura {time.strftime('%H:%M:%S', time.gmtime(songs_duration[1]/1000))}")
+# def top_artists(songs: list, artist: str) -> None:
+#     albums = {}
+#     for song in songs:
+#         if artist.lower() in song.artist.lower():
+#             artist = song.artist
+#             if song.album in albums:
+#                 albums[song.album] = (albums[song.album][0] + 1, albums[song.album][1] + float(song.duration))
+#             else:
+#                 albums[song.album] = (1, float(song.duration))
+#     print(f"{artist} have {len(albums.keys())} albums:")
+#     for album, songs_duration in albums.items():
+#         print("\u2022", f"{album} have {songs_duration[0]} songs & time {time.strftime('%H:%M:%S', time.gmtime(songs_duration[1]/1000))}")
+
+import pandas as pd
+
+def top_artists(data: pd.DataFrame) -> None:
+    data['Views'] = pd.to_numeric(data['Views'], errors='coerce')
+    artist_views = data.groupby('Artist')['Views'].sum().sort_values(ascending=False).head(10)
+    
+    print("Top 10 artists with most views:")
+    for i, (artist, views) in enumerate(artist_views.items(), start=1):
+        print(f"{i}. {artist}: {views} views")
